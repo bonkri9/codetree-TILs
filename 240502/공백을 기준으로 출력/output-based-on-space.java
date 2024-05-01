8013ea86e3b5d71583d62cf66a4867db8c047afa
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-05-02 07:38:29 +0900

    Title: 공백을 기준으로 출력, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240502/공백을 기준으로 출력/output-based-on-space.java
+++ 240502/공백을 기준으로 출력/output-based-on-space.java
@@ -2,14 +2,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] arr1 = sc.nextLine().split(" ");
-        String[] arr2 = sc.nextLine().split(" ");
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
 
-        for(int i=0;i<arr1.length;i++){
-            System.out.print(arr1[i]);
+        for(int i=0;i<str1.length();i++){
+            if(str1.charAt(i) != ' ')
+                System.out.print(str1.charAt(i));
         }
-        for(int i=0;i<arr2.length;i++){
-            System.out.print(arr2[i]);
+        for(int i=0;i<str2.length();i++){
+            if(str2.charAt(i) != ' ')
+                System.out.print(str2.charAt(i));
         }
     }
 }
