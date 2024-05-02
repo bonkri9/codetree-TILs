afe7d626079ee4529f658220a948c20d3fdc2067
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-05-02 23:11:35 +0900

    Title: 규칙에 따라 밀기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240502/규칙에 따라 밀기/push-by-the-rules.java
+++ 240502/규칙에 따라 밀기/push-by-the-rules.java
@@ -2,31 +2,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] strArr = sc.next().split("");
-        String[] command = sc.next().split("");
-        int lenStr = strArr.length;
+        String str = sc.next();
+        String command = sc.next();
+        int lenStr = str.length();
 
-        for(int i=0;i<command.length;i++){
-            if(command[i].equals("L")){
-                String tmp = strArr[0];
-                for(int j=1;j<lenStr;j++){
-                    strArr[j - 1] = strArr[j];
-                }
-                strArr[lenStr - 1] = tmp;
-                continue;
-            }
-            if(command[i].equals("R")){
-                String tmp = strArr[lenStr - 1];
-                for(int j = lenStr - 2; j >= 0; j--){
-                    strArr[j + 1] = strArr[j];
-                }
-                strArr[0] = tmp;
-                continue;
-            }
+        for(int i=0;i<command.length();i++){
+            if(command.charAt(i) == 'L') str = str.substring(1) + str.substring(0, 1);
+            else str = str.substring(lenStr - 1) + str.substring(0, lenStr - 1);
         }
 
-        for(int i=0;i<lenStr;i++){
-            System.out.print(strArr[i]);
-        }
+        System.out.print(str);
     }
 }
