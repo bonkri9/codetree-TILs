f4c554a9cdb79ba6c5a1c1d3ee374dea5b2d12cc
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-05-04 20:39:44 +0900

    Title: 아스키코드표 맞추기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240504/아스키코드표 맞추기/chart-of-ASCII.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<5;i++){
+            System.out.print((char)sc.nextInt() + " ");
+        }
+    }
+}
