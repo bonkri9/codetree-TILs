6dd7be12b11f07067bc654f2ee98487fcc25b624
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-06-03 14:19:40 +0900

    Title: 함수를 이용한 소수 판별, Time: 171ms, Memory: 10MB, Status: Passed - CodeTree

--- 240603/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 240603/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -1,6 +1,8 @@
 import java.util.*;
 public class Main {
     public static boolean isPrime(int num){
+        if(num==1) return false;
+        
         for(int i = 2;i<num;i++){
             if(num % i == 0) return false;
         }
