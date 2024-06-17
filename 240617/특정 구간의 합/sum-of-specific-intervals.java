0a38d4dcddc8dc1a9b5dc09d5c7412c9da2d4d29
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-06-17 13:45:59 +0900

    Title: 특정 구간의 합, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

--- 240617/특정 구간의 합/sum-of-specific-intervals.java
+++ 240617/특정 구간의 합/sum-of-specific-intervals.java
@@ -2,28 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n,m;
+    public static int n, m;
     public static int[] arr;
+    public static int[] prefixSum;
     public static StringTokenizer st;
     public static BufferedReader br;
 
-    public static void printSum() throws IOException{
-        for(int i=0;i<m;i++){
-            int sum = 0;
+    public static void calculatePrefixSum() {
+        prefixSum = new int[n + 1];  // 크기를 n+1로 설정하여 편의상 prefixSum[0] = 0
+        for (int i = 1; i <= n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + arr[i - 1];
+        }
+    }
 
+    public static void printSum() throws IOException {
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int start = Integer.parseInt(st.nextToken()) - 1;
             int end = Integer.parseInt(st.nextToken()) - 1;
 
-            for(int j=start;j<=end;j++){
-                sum += arr[j];
-            }
-
+            int sum = prefixSum[end + 1] - prefixSum[start];
             System.out.println(sum);
         }
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
 
         st = new StringTokenizer(br.readLine(), " ");
@@ -32,11 +35,13 @@ public class Main {
 
         arr = new int[n];
         st = new StringTokenizer(br.readLine(), " ");
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        calculatePrefixSum();  // 구간 합 배열 계산
         printSum();
         
+        br.close();
     }
 }
