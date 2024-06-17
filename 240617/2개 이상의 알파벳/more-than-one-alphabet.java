50cc92e8b10e9582c18ba248c708ad6fa5d1b2b2
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-06-17 09:27:22 +0900

    Title: 2개 이상의 알파벳, Time: 107ms, Memory: 10MB, Status: Passed - CodeTree

--- 240617/2개 이상의 알파벳/more-than-one-alphabet.java
+++ 240617/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -1,23 +1,22 @@
 import java.util.*;
 public class Main {
     public static String str = "";
-    public static int numOfAlpha = 1;
 
-    public static void countAlpha(String str){
-        for(int i=0;i<str.length()-1;i++){
-            if(str.charAt(i) != str.charAt(i+1)){
-                numOfAlpha++;
+    // 하나라도 서로 다른 알파벳 있으면 그 문자열은 알파벳 2개 이상.
+    public static boolean isMoreTwoAlpha(String str){
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) != str.charAt(0)){
+                return true;
             }
         }
+        return false;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         str = sc.next();
 
-        countAlpha(str);
-
-        if(numOfAlpha >= 2){
+        if(isMoreTwoAlpha(str)){
             System.out.print("Yes");
         }else{
             System.out.print("No");
