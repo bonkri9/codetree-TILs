bc25da5a770aa397071a9ea5dc5f67ba4f9711af
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-06-17 10:22:45 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240617/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    public static String target = "";
+    public static String object = "";
+
+    public static int findIdx(){
+        for(int i=0;i<target.length();i++){
+            for(int j=0;j<object.length();j++){
+                if(target.charAt(i+j) != object.charAt(j)){
+                    break;
+                }
+                if(j == object.length()-1) return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        target = sc.next();
+        object = sc.next();
+
+        System.out.print(findIdx());
+
+    }
+}
