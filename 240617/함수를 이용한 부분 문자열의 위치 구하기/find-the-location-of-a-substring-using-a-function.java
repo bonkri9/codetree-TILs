a19ede66a1bdb5efe874679c98868ed11e0a0c94
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-06-17 10:50:29 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240617/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
+++ 240617/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
@@ -1,26 +1,55 @@
 import java.util.*;
 public class Main {
-    public static String target = "";
-    public static String object = "";
+    public static String text, pattern;
 
-    public static int findIdx(){
-        for(int i=0;i<=target.length() - object.length();i++){
-            for(int j=0;j<object.length();j++){
-                if(target.charAt(i+j) != object.charAt(j)){
-                    break;
-                }
-                if(j == object.length()-1) return i;
+    public static boolean isSubStr(int startIdx){
+        int textLen = text.length();
+        int patternLen = pattern.length();
+
+        // 전체 text 문자열 길이보다 
+        // 탐색해야할 길이가 더 길면
+        // false 반환
+        if(startIdx > textLen - patternLen){
+            return false;
+        }
+
+        for(int i=0;i<patternLen;i++){
+            // 한 글자라도 다르면, false 반환
+            if(text.charAt(startIdx + i) != pattern.charAt(i)){
+                return false;
             }
         }
+
+        // 모두 같으면 반복문 마쳤으므로 true 반환
+        return true;
+    }
+
+
+
+    // 타겟 문자열 돌기
+    // 부분 문자열의 인덱스 반환
+    public static int findIdx(){
+        int textLen = text.length();
+
+        for(int i=0;i<textLen;i++){
+            // i번째를 시작으로 부분 문자열 만족하면, 해당 인덱스 반환
+            // 만족하지 않으면 계속 반복문 진행 
+            if(isSubStr(i)){
+                return i;
+            }
+        }
+
+        // 없으면 -1 반환
         return -1;
     }
 
     public static void main(String[] args) {
+        // 변수 선언 및 입력
         Scanner sc = new Scanner(System.in);
-        target = sc.next();
-        object = sc.next();
+        text = sc.next();
+        pattern = sc.next();
 
+        // return된 인덱스 출력
         System.out.print(findIdx());
-
     }
 }
