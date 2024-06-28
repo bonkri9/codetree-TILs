16c07c5ca8d5bf75b7d97c6202c9a26c89fb62bf
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-06-28 11:49:22 +0900

    Title: 1이 되는 순간까지, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240628/1이 되는 순간까지/until-the-moment-I-reach-one.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int cnt = 0;
+    public static int calNum(int num){
+
+        if(num == 1){
+            return cnt;
+        }
+
+        if(num % 2 == 0){
+            num /= 2;
+            cnt++;
+            return calNum(num);
+        }else{
+            num /= 3;
+            cnt++;
+            return calNum(num);
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.print(calNum(n));
+        br.close();
+    }
+}
