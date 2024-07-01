699eea4575756e8213b2e428a02334878f29e6a0
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-07-01 09:33:45 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240701/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int sumNum(int num){
+        if(num < 1) return 0;
+
+        return sumNum(num - 2) + num;
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.print(sumNum(n));
+
+    }
+}
