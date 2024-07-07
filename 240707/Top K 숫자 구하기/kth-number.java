1ae35587490ac26fc42fae000edf8716e6e66fd9
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-07-07 14:26:18 +0900

    Title: Top K 숫자 구하기, Time: 106ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240707/Top K 숫자 구하기/kth-number.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.print(arr[k-1]);
+    }
+}
