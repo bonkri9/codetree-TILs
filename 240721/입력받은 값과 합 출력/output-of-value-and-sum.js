4ae6c60f021b0306d52da0827c94abfcbfdd5899
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-07-21 09:55:00 +0900

    Title: 입력받은 값과 합 출력, Time: 166ms, Memory: 15MB, Status: Passed - CodeTree

--- 240721/입력받은 값과 합 출력/output-of-value-and-sum.js
+++ 240721/입력받은 값과 합 출력/output-of-value-and-sum.js
@@ -1,4 +1,7 @@
 const fs = require("fs");
 let arr = fs.readFileSync(0).toString().split(" ");
 
-console.log(arr[0], arr[1], Number(arr[0]) + Number(arr[1]));
+let a = Number(arr[0]);
+let b = Number(arr[1]);
+
+console.log(a, b, a+b);
