38cd3fd2fde050d7cdd8cee494d9daa84df7d8b3
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-07-23 09:44:55 +0900

    Title: 합과 평균의 차, Time: 195ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240723/합과 평균의 차/sub-of-average-and-sum.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+let sum = a + b + c;
+let avg = (a + b + c) / 3;
+let minus = sum - avg;
+
+console.log(sum);
+console.log(avg);
+console.log(minus);
