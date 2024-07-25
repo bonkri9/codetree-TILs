01d154b47f3360a1a5b49b89929d32cb0fc6b509
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-07-25 09:30:43 +0900

    Title: 공약수의 유무, Time: 185ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240725/공약수의 유무/presence-or-absence-of-a-common-divisor.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let satisfied = false;
+
+for(let i = a; i <= b; i++){
+    if(1920 % i === 0 && 2880 % i === 0)
+        satisfied = true;
+}
+
+if(satisfied) console.log(1);
+else console.log(0);
