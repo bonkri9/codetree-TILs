0e36247d80454224580ff639e850b747c9918f83
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-07-29 09:46:25 +0900

    Title: 정사각 알파벳 출력하기, Time: 214ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240729/정사각 알파벳 출력하기/print-alpabet-in-square.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let alpha = 'A';
+let str = "";
+
+for(let i=0;i<n;i++){
+    for(let j=0;j<n;j++){
+        str += alpha;
+        let index = alpha.charCodeAt();
+        alpha = String.fromCharCode(index++);
+    }
+    str += "\n";
+}
+
+console.log(str);
