529b094738128f2b1883d4520fff2770b9d867e1
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-07-30 17:34:05 +0900

    Title: 병합 정렬 구현, Time: 477ms, Memory: 22MB, Status: Passed - CodeTree

--- 240730/병합 정렬 구현/implement-merge-sort.java
+++ 240730/병합 정렬 구현/implement-merge-sort.java
@@ -2,43 +2,46 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int[] mergedArr;
+    public static final int MAX_N = 100000;
+
+    public static int n;
+    public static int[] arr = new int[MAX_N];
+    public static int[] mergedArr = new int[MAX_N];
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
 
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++){
             arr[i] = Integer.parseInt(st.nextToken());
         } // 입력 끝
 
-        mergedArr = new int[n];
-        mergeSort(arr, 0, n - 1);
+        mergeSort(0, n - 1);
 
         StringBuilder sb = new StringBuilder();
-        for(int num : arr){
-            sb.append(num).append(" ");
+        for(int i=0;i<n;i++){
+            sb.append(arr[i]).append(" ");
         }
 
         System.out.println(sb.toString().trim());
     }
 
-    public static int[] mergeSort(int[] arr, int low, int high){
+    public static int[] mergeSort(int low, int high){
         if(low < high){
             int mid = (low + high) / 2;
-            mergeSort(arr, low, mid); // 왼쪽 부분 리스트 정렬
-            mergeSort(arr, mid + 1, high); // 오른쪽 부분 리스트 정렬
-            merge(arr, low, mid, high); // 병합
+            mergeSort(low, mid); // 왼쪽 부분 리스트 정렬
+            mergeSort(mid + 1, high); // 오른쪽 부분 리스트 정렬
+            merge(low, mid, high); // 병합
         }
 
         return arr;
     }
 
-    public static void merge(int[] arr, int low, int mid, int high){
+    public static void merge(int low, int mid, int high){
         int i = low;
         int j = mid + 1;
         int k = low;
@@ -46,33 +49,25 @@ public class Main {
         // 두 부분 리스트를 병합
         while(i <= mid && j <= high){
             if(arr[i] <= arr[j]){
-                mergedArr[k] = arr[i];
-                k++;
-                i++;
+                mergedArr[k++] = arr[i++];
             }else{
-                mergedArr[k] = arr[j];
-                k++;
-                j++;
+                mergedArr[k++] = arr[j++];
             }
         }
 
         // 왼쪽 리스트의 남은 원소들을 복사 
         while(i <= mid){
-            mergedArr[k] = arr[i];
-            k++;
-            i++;
+            mergedArr[k++] = arr[i++];
         }
 
         // 오른쪽 리스트의 남은 원소들을 복사 
         while(j <= high){
-            mergedArr[k] = arr[j];
-            k++;
-            j++;
+            mergedArr[k++] = arr[j++];
         }
 
         // 병합된 결과를 원래 배열에 복사 
-        for(k = low; k <= high; k++){
-            arr[k] = mergedArr[k];
+        for(int l = low; l <= high; l++){
+            arr[l] = mergedArr[l];
         }
     }
 }
