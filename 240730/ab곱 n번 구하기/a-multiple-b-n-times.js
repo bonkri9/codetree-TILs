79c86dafe6b3c5e650ab76c5bc67332dd5502bdf
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-07-30 16:36:53 +0900

    Title: ab곱 n번 구하기, Time: 177ms, Memory: 14MB, Status: Passed - CodeTree

--- 240730/ab곱 n번 구하기/a-multiple-b-n-times.js
+++ 240730/ab곱 n번 구하기/a-multiple-b-n-times.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
 
-let n = input[0];
+let n = Number(input[0]);
 
 for(let i=1;i<=n;i++){
     let line = input[i].split(" ");
-    let a = line[0];
-    let b = line[1];
+    let a = Number(line[0]);
+    let b = Number(line[1]);
     let sum = 1;
 
     for(let j=a;j<=b;j++){
