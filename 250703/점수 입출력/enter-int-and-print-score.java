da1f81ed8341463bd1156a407320f57c75365d1c
Bonkri <139411343+bonkri9@users.noreply.github.com>
2025-07-03 13:25:42 +0900

    Title: 점수 입출력, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250703/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.printf("Your score is %d point.", a);
+    }
+}
