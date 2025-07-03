413a75fbc828cdbf2a18db9c289f950e1d81db00
Bonkri <139411343+bonkri9@users.noreply.github.com>
2025-07-03 13:33:16 +0900

    Title: 실수 입력받아 계산 2, Time: 158ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250703/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.printf("%.2f", a + 1.5);
+    }
+}
