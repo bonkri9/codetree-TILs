a34c53cb056980b026beb7141b0e0fbc7284d242
Bonkri <139411343+bonkri9@users.noreply.github.com>
2025-07-03 19:45:47 +0900

    Title: 문자열 순서 바꾸기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250703/문자열 순서 바꾸기/change-order-of-strings.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.next(), T = sc.next();
+        System.out.printf("%s\n%s", T, S);
+    }
+}
