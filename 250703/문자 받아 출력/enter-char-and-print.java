49496fbce8cd3cd4d556bccd012642e08180d241
Bonkri <139411343+bonkri9@users.noreply.github.com>
2025-07-03 19:31:57 +0900

    Title: 문자 받아 출력, Time: 135ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250703/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        System.out.print(ch);
+    }
+}
