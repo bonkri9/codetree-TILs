24175fe7f6138ac61959995b3b667f4a2eac6d52
Bonkri <139411343+bonkri9@users.noreply.github.com>
2025-07-03 20:19:59 +0900

    Title: 입력받아 출력 2, Time: 112ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250703/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] strArr = sc.next().split("-");
+        System.out.print(strArr[0] + strArr[1]);
+    }
+}
