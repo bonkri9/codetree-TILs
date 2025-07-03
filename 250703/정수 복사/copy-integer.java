9a1be227d4264125af346be9114a96179187fdd8
Bonkri <139411343+bonkri9@users.noreply.github.com>
2025-07-03 13:03:34 +0900

    Title: 정수 복사, Time: 94ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250703/정수 복사/copy-integer.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        b = a;
+        System.out.printf("%d %d\n%d", a, b, a * b);
+    }
+}
