712bb25950880ba227f5079c88cc68ab5f74eac3
Bonkri <139411343+bonkri9@users.noreply.github.com>
2025-07-04 13:12:45 +0900

    Title: 합과 차의 나눗셈, Time: 157ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250704/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble(), b = sc.nextDouble();
+        System.out.printf("%.2f", (a+b) / (a-b));
+    }
+}
