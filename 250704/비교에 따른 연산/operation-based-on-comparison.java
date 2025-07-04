36049ea2e5d089379d0506d064a4575c03080f4c
Bonkri <139411343+bonkri9@users.noreply.github.com>
2025-07-04 13:24:22 +0900

    Title: 비교에 따른 연산, Time: 134ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250704/비교에 따른 연산/operation-based-on-comparison.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        if (a > b) {
+            System.out.print(a * b);
+        } else {
+            System.out.print(b / a);
+        }
+    }
+}
